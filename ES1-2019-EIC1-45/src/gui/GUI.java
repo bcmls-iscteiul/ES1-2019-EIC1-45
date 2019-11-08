666ad348adb7e30d6e5340ca471dc4bc1ddda01d
joaof <joaof@172.17.12.170>
2019-11-08 14:55:57 +0000

    GUI buttons and layout finished

--- ES1-2019-EIC1-45/src/gui/GUI.java
+++ ES1-2019-EIC1-45/src/gui/GUI.java
@@ -10,6 +10,9 @@ import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class GUI {
 
@@ -44,30 +47,56 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 626, 416);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Qualidade iPlasma");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel_1.setLayout(new GridLayout(3, 0, 0, 0));
 		panel_1.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("Qualidade PMD");
 		panel_1.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnNewButton_2 = new JButton("Regras User");
 		panel_1.add(btnNewButton_2);
+		
+		
 		
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2);
 		
-		table = new JTable();
+		
+		String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+		@SuppressWarnings("deprecation")
+		Object[][] data = {
+			    {"Kathy", "Smith",
+			     "Snowboarding", 5, false},
+			    {"John", "Doe",
+			     "Rowing", 3, true},
+			    {"Sue", "Black",
+			     "Knitting", 2, false},
+			    {"Jane", "White",
+			     "Speed reading", 20, true},
+			    {"Joe", "Brown",
+			     "Pool", 10, false}
+			};
+		table = new JTable(data, columnNames);
 		panel_2.add(table);
 	}
 
