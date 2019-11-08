a8f114abfa4b8b02383301a16aac602d541a4f60
fhcpc-iscteiul <fhcpc@iscte-iul.pt>
2019-11-08 14:43:33 +0000

    Classe InfoObj criada e ExcelObject alterada para ser uma ArryList de InfoObj.

---
+++ ES1-2019-EIC1-45/src/excel/InfoObj.java
@@ -0,0 +1,37 @@
+package excel;
+
+public class InfoObj {
+	
+	String method;
+	int loc;
+	int cyclo;
+	int atfd;
+	double laa;
+	
+	public InfoObj(ExcelFile f, int i) {
+	
+	
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public int getLoc() {
+		return loc;
+	}
+
+	public int getCyclo() {
+		return cyclo;
+	}
+
+	public int getAtfd() {
+		return atfd;
+	}
+
+	public double getLaa() {
+		return laa;
+	}
+	
+	
+}
