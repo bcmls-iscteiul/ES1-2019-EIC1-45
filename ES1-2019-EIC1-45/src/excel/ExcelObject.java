02800663ed4eba3d3ae10b0f1c8b72a01f86686f
fhcpc-iscteiul <fhcpc@iscte-iul.pt>
2019-11-08 11:45:02 +0000

    Objecto ExcelObject criada. Falta instanciar os atributos da classe, method; loc; cyclo, atfd e laa, pois ficheiro ainda não importado. Métodos getters dos atributos implementados.

--- ES1-2019-EIC1-45/src/excel/ExcelObject.java
+++ ES1-2019-EIC1-45/src/excel/ExcelObject.java
@@ -2,4 +2,33 @@ package excel;
 
 public class ExcelObject {
 
+	String method;
+	int loc;
+	int cyclo;
+	int atfd;
+	double laa;
+	
+	public ExcelObject(ExcelFile f) {
+		//atributos são inicializados aqui baseados na info do ficheiro importado.
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public int getLoc() {
+		return loc;
+	}
+
+	public int getCyclo() {
+		return cyclo;
+	}
+
+	public int getAtfd() {
+		return atfd;
+	}
+
+	public double getLaa() {
+		return laa;
+	}
 }
