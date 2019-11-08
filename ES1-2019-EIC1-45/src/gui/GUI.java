d609281b96c4d1cbc42c3f6f8c3e19804a7cbeee
joaof <joaof@172.17.22.46>
2019-11-08 11:59:24 +0000

    Basic Layout added (Without descriptions or custom name vars)

--- ES1-2019-EIC1-45/src/gui/GUI.java
+++ ES1-2019-EIC1-45/src/gui/GUI.java
@@ -5,10 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.GridLayout;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JTable;
 
 public class GUI {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -44,6 +50,25 @@ public class GUI {
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel_1.add(btnNewButton_2);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		
+		table = new JTable();
+		panel_2.add(table);
 	}
 
 }
