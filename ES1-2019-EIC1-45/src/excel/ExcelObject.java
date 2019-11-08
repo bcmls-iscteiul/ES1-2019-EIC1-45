34eb8a21a0c3ae962a1c55295516bf7df816bc2b
Nuno Rego <nuno.rego@outlook.pt>
2019-11-08 18:12:33 +0000

    Começo das classes para tratamento do Excel

--- ES1-2019-EIC1-45/src/excel/ExcelObject.java
+++ ES1-2019-EIC1-45/src/excel/ExcelObject.java
@@ -1,15 +1,35 @@
 package excel;
 
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelObject {
 
-	String method;
-	int loc;
-	int cyclo;
-	int atfd;
-	double laa;
+	private XSSFWorkbook workbook;
+	private String method;
+	private int loc;
+	private int cyclo;
+	private int atfd;
+	private double laa;
 	
-	public ExcelObject(ExcelFile f) {
-		//atributos são inicializados aqui baseados na info do ficheiro importado.
+	public ExcelObject(XSSFWorkbook workbook) {
+		this.workbook = workbook;
 	}
 
 	public String getMethod() {
