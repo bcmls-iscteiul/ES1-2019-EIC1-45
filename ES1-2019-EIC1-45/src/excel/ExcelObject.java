beef1d58d7b5710fdef44234444025fe16f33d0e
fhcpc-iscteiul <fhcpc@iscte-iul.pt>
2019-11-12 09:55:08 +0000

    Deleted unnecessary class

--- ES1-2019-EIC1-45/src/excel/ExcelObject.java
+++ ES1-2019-EIC1-45/src/excel/ExcelObject.java
@@ -6,17 +6,10 @@ import java.util.List;
 
 public class ExcelObject {
 	
-	InfoObj i;
-	List<InfoObj> list = new ArrayList<InfoObj>();
 	
 	public ExcelObject(ExcelFile f) {
 		
-		//atributos são inicializados aqui baseados na info do ficheiro importado.
-		for(int l=0; l<10; l++/*substituir por n de linhas do ficheir*/) {
-			i = new InfoObj(f,l);
-			list.add(i);
-		}
+		//atributos são inicializados aqui baseados na info do ficheiro importado e funcoes do EscellFile.
 	}
-	
 	
 }
