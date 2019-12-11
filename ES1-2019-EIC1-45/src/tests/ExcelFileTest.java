62f0dbf6dc23d26d0842937727a31cbcf3da22a7
tmmgo-iscteiul <tmmgo@iscte-iul.pt>
2019-12-11 16:35:10 +0000

    setUp() in ExcelFileTest

---
+++ ES1-2019-EIC1-45/src/tests/ExcelFileTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import excel.ExcelFile;
+import excel.ExcelObject;
+
+class ExcelFileTest {
+
+	
+	ExcelFile file;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		file = new ExcelFile();
+	}
+	
+	
+	
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
